f285dcc60a172bc7d2d11fc21316e4142c6f8ae7
Nikhil <nik.05.jindal@gmail.com>
2022-08-24 20:19:32 +0530

    Support for web added

---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static String apiKey = "AIzaSyDKYWBV-cE2F_M54lFGQNqC4ZB5bJV5Sn8";
+  static String authDomain = "chat-app-flutter-nik.firebaseapp.com";
+  static String projectId = "chat-app-flutter-nik";
+  static String storageBucket = "chat-app-flutter-nik.appspot.com";
+  static String messagingSenderId = "801430636082";
+  static String appId = "1:801430636082:web:6fd77e3e11014d18d59e2b";
+}
