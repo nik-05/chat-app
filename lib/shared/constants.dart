d97a9ae51d85898048747b453fcd4eb57ecaf4ba
Nikhil <nik.05.jindal@gmail.com>
2022-08-27 20:57:37 +0530

    Login Page UI - Design

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,4 +1,4 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
 
 class Constants {
   static String apiKey = "AIzaSyDKYWBV-cE2F_M54lFGQNqC4ZB5bJV5Sn8";
@@ -7,5 +7,6 @@ class Constants {
   static String storageBucket = "chat-app-flutter-nik.appspot.com";
   static String messagingSenderId = "801430636082";
   static String appId = "1:801430636082:web:6fd77e3e11014d18d59e2b";
+  final primaryColor = const Color(0xFFee7b64);
 
 }
